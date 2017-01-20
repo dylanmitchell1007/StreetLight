4a9d72f08d37bd71cd3d38ed258d132bbecbc044
dylanmitchell1007 <armadummy@yahoo.com>
2017-01-19 18:00:23 -0600

    New

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetLight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FSM app = new FSM();
+            
+
+           
+
+
+        }
+    }
+}
---
+++ State.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace StreetLight
+{
+        public enum Lights
+        {
+            Init = 0,
+             Red = 1,
+             Yellow = 2,
+             Green = 3,
+             Exit = 4,
+        }
+
+
+   public class State
+    {
+        public Enum name;
+
+        public Lights state;
+
+        Stopwatch time;
+       
+
+
+    }
+}
